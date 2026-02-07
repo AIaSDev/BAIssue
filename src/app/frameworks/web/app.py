99a5d2989c5a3f414b2f73fa0013c1a89de5f1ed
Andreas Martin <andreas.martin@andreasmartin.ch>
2026-02-07 16:46:57 +0100

    Import fix

--- src/app/frameworks/web/app.py
+++ src/app/frameworks/web/app.py
@@ -2,7 +2,7 @@
 from fastapi import FastAPI
 
 from app.core.config import API_TITLE, API_VERSION, API_DESCRIPTION
-from app.interfaces.controllers.issues_api import router as issues_router
+from app.interfaces.controllers.issue_api import router as issues_router
 
 
 def create_app(init_db: bool = True) -> FastAPI:
